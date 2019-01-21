a3ab72f4e4dc89f0e3aff97ce43e5a7fca9357c6
mike <michaeldotson@protonmail.com>
2019-01-20 23:57:42 -0800

    adds todays homework- error on ruby version, unable to load server for testing

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,11 +1,30 @@
 class Api::ActorsController < ApplicationController
-  def single_actor
-    @actor = Actor.first
+#   def single_actor
+#     @actor = Actor.first
+#     render 'single_actor.json.jbuilder'
+#   end
+
+#   def all_actors
+#     @actors = Actor.all
+#     render 'all_actors.json.jbuilder'
+#   end
+# end
+
+
+###########
+def single_actor_query
+    @actor = params["actor"]
     render 'single_actor.json.jbuilder'
   end
 
-  def all_actors
-    @actors = Actor.all
-    render 'all_actors.json.jbuilder'
+  def single_actor_segment
+    @actor = params["wildcard"]
+    render 'single_actor.json.jbuilder'
+  end
+
+  def single_actor_body
+    @actor = params[:actor]
+    @message = "The award goes to #{@actor}"
+    render 'single_actor.json.jbuilder'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-  get "single_actor" => "actors#single_actor"
+  get "/single_actor_query" => "actors#single_actor_query"
+  get "/single_actor_segment/:actor" => "actors#single_actor_segment"
+  post "/single_actor_body" => "actors#single_actor_body"
+
   get "all_actors" => "actors#all_actors"
-  
   get "single_movie" => "movies#single_movie"
   get "all_movies" => "movies#all_movies"
   end
