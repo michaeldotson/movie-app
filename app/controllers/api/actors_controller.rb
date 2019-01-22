5e9f004ae1e4ffa0414df87a3abe06f46378adfe
mike <michaeldotson@protonmail.com>
2019-01-22 00:04:50 -0800

    add today's homework

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,42 +1,42 @@
 class Api::ActorsController < ApplicationController
   class Api::ActorsController < ApplicationController
 
-  # def index
-  #   @actors = Actor.all
-  #   render 'index.json.jbuilder'
-  # end
+  def index
+    @actors = Actor.all
+    render 'index.json.jbuilder'
+  end
 
-  # def show
-  #   @actor = Actor.find_by(id: params["id"])
-  #   render 'show.json.jbuilder'
-  # end
+  def show
+    @actor = Actor.find_by(id: params["id"])
+    render 'show.json.jbuilder'
+  end
 
-  # def create
-  #   @actor = Actor.new(
-  #     first_name: params["first_name"],
-  #     last_name: params["last_name"],
-  #     known_for: params["known_for"]
-  #     )
+  def create
+    @actor = Actor.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"]
+      )
 
-  #     @actor.save
-  #     render 'show.json.jbuilder'
-  # end
+      @actor.save
+      render 'show.json.jbuilder'
+  end
 
-  # def update
-  #   @actor = Actor.find_by(id: params["id"])
-  #   @actor.first_name = params["first_name"] || @actor.first_name
-  #     @actor.last_name = params["last_name"] || @actor.last_name
-  #     @actor.known_for = params["known_for"] || @actor.known_for
+  def update
+    @actor = Actor.find_by(id: params["id"])
+    @actor.first_name = params["first_name"] || @actor.first_name
+      @actor.last_name = params["last_name"] || @actor.last_name
+      @actor.known_for = params["known_for"] || @actor.known_for
 
-  #     @actor.save
-  #   render 'show.json.jbuilder'
-  # end
+      @actor.save
+    render 'show.json.jbuilder'
+  end
 
-  # def destroy
-  #   @actor = Actor.find_by(id: params["id"])
-  #   @actor.destroy
-  #   render json: {message: "#{@actor.first_name} successfully deleted."}
-  # end
+  def destroy
+    @actor = Actor.find_by(id: params["id"])
+    @actor.destroy
+    render json: {message: "#{@actor.first_name} successfully deleted."}
+  end
 
   def index_movie
     @movies = Movie.all
